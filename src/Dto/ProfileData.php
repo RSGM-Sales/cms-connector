3ea106a9f8a9ca0be42920e30e37a34885bc8f6d
benjamin.vansteelandt@flowsparks.com <benjamin.vansteelandt@flowsparks.com>
2023-04-14 16:36:48 +0200

    updaded profile data

--- src/Dto/ProfileData.php
+++ src/Dto/ProfileData.php
@@ -5,9 +5,17 @@ namespace RSGMSales\Connector\Dto;
 class ProfileData
 {
     public bool $marketingOptIn;
+    public string $oldPassword;
+    public string $newPassword;
+    public string $oldUsername;
+    public string $newUsername;
 
-    public function __construct(bool $marketingOptIn = true)
+    public function __construct(bool $marketingOptIn = null, string $oldPassword = null, string $newPassword = null, string $oldUsername = null, string $newUsername = null)
     {
         $this->marketingOptIn = $marketingOptIn;
+        $this->oldPassword = $oldPassword;
+        $this->newPassword = $newPassword;
+        $this->oldUsername = $oldUsername;
+        $this->newUsername = $newUsername;
     }
 }

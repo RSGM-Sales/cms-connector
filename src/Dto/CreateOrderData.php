7cc3fbdc2cea1b81e93f37644b9bbe987a165e55
benjamin.vansteelandt@flowsparks.com <benjamin.vansteelandt@flowsparks.com>
2023-06-08 15:31:57 +0200

    updated create order action with latest fields

--- src/Dto/CreateOrderData.php
+++ src/Dto/CreateOrderData.php
@@ -4,35 +4,43 @@ namespace RSGMSales\Connector\Dto;
 
 class CreateOrderData
 {
-
-    public int $productId;
+    public string $inGameName;
 
     /**
-     * The amount of mills (/product) that the user would like to order
-     * @var float|int
+     * The URL to which the user should be redirected after the payment flow has been completed (by default this should be their order history page)
+     * @var string
      */
-    public float $amount;
+    public string $siteRedirectUrl;
 
     /**
      * The ID of the currency that the user would like to use to pay for their order
      * @var int
      */
     public int $currencyId;
-    public string $inGameName;
-    public string $coupon;
-    public int $paymentMethodId;
+    public int $paymentProviderPaymentMethodId;
+    public int $productId;
 
-    public function __construct(int $productId, float $amount, int $currencyId, string $inGameName, int $paymentMethodId, string $coupon = '')
+    /**
+     * The amount of product (mills) that the user would like to order
+     * @var float|int
+     */
+    public float $amount;
+    public string $coupon;
+    public array $metaData;
+
+    public function __construct(string $inGameName, string $siteRedirectUrl, int $currencyId, int $paymentProviderPaymentMethodId, int $productId, float $amount, string $coupon = '', array $metaData = [])
     {
+        $this->inGameName = $inGameName;
+        $this->siteRedirectUrl = $siteRedirectUrl;
+        $this->currencyId = $currencyId;
+        $this->paymentProviderPaymentMethodId = $paymentProviderPaymentMethodId;
         $this->productId = $productId;
         $this->amount = $amount;
-        $this->currencyId = $currencyId;
-        $this->inGameName = $inGameName;
         $this->coupon = $coupon;
-        $this->paymentMethodId = $paymentMethodId;
+        $this->metaData = $metaData;
     }
 
-    public static function create(int $productId, float $amount, int $currencyId, string $runescapeName, int $paymentMethodId, string $coupon = ''): CreateOrderData {
-        return new CreateOrderData($productId, $amount, $currencyId, $runescapeName, $paymentMethodId, $coupon);
+    public static function create(string $inGameName, string $siteRedirectUrl, int $currencyId, int $paymentProviderPaymentMethodId, int $productId, float $amount, string $coupon = '', array $metaData = []): CreateOrderData {
+        return new CreateOrderData($inGameName, $siteRedirectUrl, $currencyId, $paymentProviderPaymentMethodId, $productId, $amount, $coupon, $metaData);
     }
 }

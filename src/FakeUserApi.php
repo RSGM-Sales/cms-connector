4568e4b8686ed0c60c248e1cbdb26e8f5dec637c
Matiangolo <mathias.vermeerschfit@hotmail.com>
2023-06-10 22:50:05 +0200

    Fix OrderHistory not paginated (might be temporary but we're getting there)

--- src/FakeUserApi.php
+++ src/FakeUserApi.php
@@ -15,39 +15,48 @@ class FakeUserApi implements UserApiInterface
     {
         return new BaseApiResponse();
     }
+
     public function createOrder(CreateOrderData $orderData): BaseApiResponse
     {
         return new BaseApiResponse();
     }
+
     public function getOrderHistory(int $page = 0): OrderHistoryApiResponse
     {
         $data = [];
 
         for ($i = 0; $i < 6; $i++) {
             $data[] = (object)[
-                'id' => $i,
-                'attributes' => (object)[
-                    'date' => fake()->date,
-                    'amount' => rand(2, 5),
-                    'total' => rand(12, 20),
-                    'status' => fake()->randomElement(['pending', 'delivered', 'failed'])
-                ],
-                'relationships' => (object)[
-                    'currency' => (object)[ "id" => 1, "attributes" => (object)["name" => "Euro", "code" => "EUR", "rate" => 1, "symbol" => "€"] ],
-                    'product' => (object)[ "id" => 1, "attributes" => (object)["name" => "RS3", "price" => fake()->randomFloat(2,1,5)] ]
-                ],
+                'type' => 'orderProduct',
+                'attributes' => [
+                    'type' => 'orderProduct',
+                    'attributes' => [
+                        'amount' => fake()->randomFloat(2, 1, 10),
+                        'status' => fake()->randomElement(['initiated', 'delivered', 'failed']),
+                        'orderNumber' => fake()->randomNumber(8),
+                        'currencyIsoCode' => fake()->currencyCode(),
+                        'currencyName' => fake()->randomElement(['euro', 'dollar', 'yen']),
+                        'providerName' => fake()->randomElement(['visa', 'bancontact', 'gtA']),
+                        'providerFee' => fake()->randomNumber(2),
+                        'couponCode' => fake()->randomNumber(4),
+                        'couponAmount' => fake()->randomNumber(2),
+                        'createdAt' => fake()->date(),
+                    ]
+                ]
             ];
         }
 
-        return new OrderHistoryApiResponse(0,0,200, "", (object)[
+        return new OrderHistoryApiResponse(0, 0, 200, "", (object)[
             'data' => $data
         ]);
     }
+
     public function logout(): BaseApiResponse
     {
         session(['user-api-token' => null]);
         return new BaseApiResponse();
     }
+
     public function updateProfile(UserPreferencesData $profileData): BaseApiResponse
     {
         return new BaseApiResponse();
